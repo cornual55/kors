9d5d60521ff1225bb0183a54794cdb9f580e1aae
Anatoly <34132497+cornual55@users.noreply.github.com>
2023-04-12 08:51:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/kors'
+  base: 'https://cornual55.github.io/kors/'
 })
